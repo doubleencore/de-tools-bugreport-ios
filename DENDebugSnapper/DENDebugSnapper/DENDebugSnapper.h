f4f7e6f88b941d52bb01026ba6035743d0e02c6b
Brad Dillon <jbradforddillon@gmail.com>
2014-04-08 16:41:39 -0400

    Initial DENDebugSnapper commit

---
+++ DENDebugSnapper/DENDebugSnapper/DENDebugSnapper.h
@@ -0,0 +1,13 @@
+//
+//  DENDebugSnapper.h
+//  DENDebugSnapper
+//
+//  Created by Brad Dillon on 4/8/14.
+//  Copyright (c) 2014 Double Encore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DENDebugSnapper : NSObject
+
+@end
---
+++ DENDebugSnapper/DENDebugSnapper/DENDebugSnapper.m
@@ -0,0 +1,13 @@
+//
+//  DENDebugSnapper.m
+//  DENDebugSnapper
+//
+//  Created by Brad Dillon on 4/8/14.
+//  Copyright (c) 2014 Double Encore. All rights reserved.
+//
+
+#import "DENDebugSnapper.h"
+
+@implementation DENDebugSnapper
+
+@end
